c007f93a7d9a39866155c6525770f802f9675b2c
Nikita <20_nik_05@mail.ru>
2025-03-03 22:20:49 +0300

    feat: добавлена валидация на бэке

--- app/submit-form.php
+++ app/submit-form.php
@@ -5,6 +5,16 @@
     use PhpOffice\PhpSpreadsheet\Spreadsheet;
     use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
+    function validation($data, $pattern) {
+        return preg_match($pattern, $data) === 1;
+    }
+
+    function clean_input($value) {
+        $value = preg_replace('/([^\wа-яА-ЯёЁ])\1+/u', '$1', $value);
+        $value = preg_replace('/^[^a-zA-ZА-Яа-яёЁ0-9]+/u', '', $value);
+        return $value;
+    }
+
     $data_file_xlsx = '/var/www/html/data/data_file.xlsx';
 
     if (file_exists($data_file_xlsx)) {
@@ -18,21 +28,94 @@
         $sheet->fromArray($headers, null, 'A1');
     }
 
+    $patterns = [
+        'book' => '/^[a-zA-ZА-Яа-яёЁ0-9\s.,№"\'-()]+$/u',
+        'author' => '/^[a-zA-ZА-Яа-яёЁ\s.,-]+$/u',
+        'year' => '/^\d{1,4}$/',
+        'publisher' => '/^[a-zA-ZА-Яа-яёЁ0-9\s.,-]+$/u',
+        'isbn' => '/^\d{13}$/',
+        'pages' => '/^\d+$/',
+        'age' => '/^\d+$/',
+        'weight' => '/^\d+(\.\d{1,2})?$/',
+        'price' => '/^\d+(\.\d{1,2})?$/',
+        'summary' => '/^[a-zA-ZА-Яа-яёЁ0-9\s.,!?"\'-]+$/u'
+    ];
+
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $book = trim($_POST['book'] ?? '');
-        $author = trim($_POST['author'] ?? '');
-        $year = trim($_POST['year'] ?? '');
-        $publisher = trim($_POST['publisher'] ?? '');
-        $isbn = trim($_POST['isbn'] ?? '');
-        $pages = trim($_POST['pages'] ?? '');
-        $age = trim($_POST['age'] ?? '');
-        $release_date = trim($_POST['release_date'] ?? '');
-        $weight = trim($_POST['weight'] ?? '');
-        $price = trim($_POST['price'] ?? '');
-        $summary = trim($_POST['summary'] ?? '');
-        
+        $book = $_POST['book'] ?? '';
+        $author = $_POST['author'] ?? '';
+        $year = $_POST['year'] ?? '';
+        $publisher = $_POST['publisher'] ?? '';
         $genres = $_POST['genre'] ?? [];
+        $isbn = $_POST['isbn'] ?? '';
+        $pages = $_POST['pages'] ?? '';
+        $age = $_POST['age'] ?? '';
+        $release_date = $_POST['release_date'] ?? '';
+        $weight = $_POST['weight'] ?? '';
+        $price = $_POST['price'] ?? '';
+        $summary = $_POST['summary'] ?? '';
         $genres_str = implode(', ', $genres);
+        
+        if ($age == "") {
+            $age = 'Не указано';
+        }
+        if ($genres_str == "") {
+            $genres_str = 'Не указано';
+        }
+        if ($summary == "") {
+            $summary = 'Не указано';
+        }
+
+        $book = clean_input($book);
+        $author = clean_input($author);
+        $year = clean_input($year);
+        $publisher = clean_input($publisher);
+        $isbn = clean_input($isbn);
+        $pages = clean_input($pages);
+        $release_date = clean_input($release_date);
+        $weight = clean_input($weight);
+        $price = clean_input($price);
+        $summary = clean_input($summary);
+
+        $message = "";
+
+        if (!validation($book, $patterns['book'])) {
+            $message .= " книга,";
+        }
+        if (!validation($author, $patterns['author'])) {
+            $message .= " автор,";
+        }
+        if (!validation($year, $patterns['year'])) {
+            $message .= " год издания,";
+        }
+        if (!validation($publisher, $patterns['publisher'])) {
+            $message .= " издательство,";
+        }
+        if (!validation($isbn, $patterns['isbn'])) {
+            $message .= " ISBN,";
+        }
+        if (!validation($pages, $patterns['pages'])) {
+            $message .= " количество страниц,";
+        }
+        if (!validation($weight, $patterns['weight'])) {
+            $message .= " вес,";
+        }
+        if (!validation($price, $patterns['price'])) {
+            $message .= " стоимость,";
+        }
+        if (!validation($summary, $patterns['summary'])) {
+            $message .= " описание,";
+        }
+        
+        if ($message !== "") {
+            $message = rtrim($message, ',');
+            echo $message;
+            // echo "<script>
+            //         alert('Некорректные данные в полях: $message! Форма будет очищена');
+            //         window.location.href = 'http://localhost:8080';
+            //     </script>";
+            exit();
+        }
 
         $lastRow = $sheet->getHighestRow() + 1;
         $sheet->fromArray([$book, $author, $genres_str, $year, $publisher, $isbn, $pages, $age, $release_date, $weight, $price, $summary], null, "A$lastRow");
