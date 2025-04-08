164f5060926c5c2df469f51f9cb20e7ae2424e83
Nikita <20_nik_05@mail.ru>
2025-04-08 15:03:48 +0300

    feat: обновлена логика взаимодействия на клиенте (script.js)

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,23 +1,47 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const dropdownButton = document.getElementById('dropdownButton');
-    const dropdownMenu = document.getElementById('dropdownMenu');
+    const dropdownButtonGenre = document.getElementById('drop-button-genre');
+    const dropdownMenuGenre = document.getElementById('dropdown-menu-genre');
 
-    dropdownButton.addEventListener('click', function(event) {
+    dropdownButtonGenre.addEventListener('click', function(event) {
         event.preventDefault();
-        dropdownMenu.classList.toggle('open');
+        dropdownMenuGenre.classList.toggle('open');
     });
 
-    const dropdownButton2 = document.getElementById('dropdownButton2');
-    const dropdownMenu2 = document.getElementById('dropdownMenu2');
+    const dropdownButtonAge = document.getElementById('drop-button-age');
+    const dropdownMenuAge = document.getElementById('dropdown-menu-age');
     
-    dropdownButton2.addEventListener('click', function(event) {
+    dropdownButtonAge.addEventListener('click', function(event) {
         event.preventDefault();
-        dropdownMenu2.classList.toggle('open');
+        dropdownMenuAge.classList.toggle('open');
     });
 
-    window.Select_Option = function(value) {
+    Select_Option = function(value) {
         document.getElementById('selected-option').textContent = value;
         document.getElementById('selected-age').value = value;
-        dropdownMenu2.classList.remove('open');
+
+        const labels = dropdownMenuAge.querySelectorAll('label');
+        labels.forEach(label => {
+            const input = label.querySelector('input[type="radio"]');
+            if (input.value === value) {
+                input.checked = true;
+                label.classList.add('selected');
+            } else {
+                input.checked = false;
+                label.classList.remove('selected');
+            }
+        });
+
+        dropdownMenuAge.classList.remove('open');
     };
+
+    document.addEventListener('click', function (event) {
+        if (!dropdownButtonGenre.contains(event.target) && !dropdownMenuGenre.contains(event.target)) {
+            dropdownMenuGenre.classList.remove('open');
+        }
+        if (!dropdownButtonAge.contains(event.target) && !dropdownMenuAge.contains(event.target)) {
+            dropdownMenuAge.classList.remove('open');
+        }
+    });
+
+    
 });
